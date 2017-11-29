007e70827ede3af5b0e275c66756787892342935
linmq <599037815@qq.com>
2017-11-29 22:49:15 +0800

    (+)

--- src/main/java/com/sjoner/http/converter/GsonResponseConverter.kt
+++ src/main/java/com/sjoner/http/converter/GsonResponseConverter.kt
@@ -5,14 +5,13 @@ import okhttp3.ResponseBody
 import java.lang.reflect.Type
 
 class GsonResponseConverter:Converter<ResponseBody?,Any> {
-    override fun convert(value: ResponseBody?, returnType: Type): Any {
+    override fun convert(value: ResponseBody?, type: Type): Any {
         val gson = Gson()
         val json = value?.string()
-        println("returnType = ${returnType.typeName}")
         if (json != null) {
-            return gson.fromJson(json, returnType)
+            return gson.fromJson(json, type)
         } else {
-            return Any()
+            return Class.forName((type as Class<*>).name).newInstance()
         }
     }
 }
